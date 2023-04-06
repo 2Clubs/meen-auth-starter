f96b53248e7c17f71a5b46abc6c7ecf4b317c9c4
2clubs <bennettdan85@gmail.com>
2023-04-06 16:06:29 -0400

    adds first commit thru registration route

---
+++ controllers/users.js
@@ -0,0 +1,20 @@
+const bcrypt = require("bcrypt")
+const express = require("express")
+const userRouter = express.Router()
+const User = require("../models/user")
+
+
+
+
+// New (registration page)
+
+// Create (registration route)
+userRouter.post('/', (req, res) => {
+    //overwrite the user password with the hashed password, then pass that in to our database
+    req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+    const createdUser = new User(req.body)
+    createdUser.save().then(res.redirect("/users"))
+});
+
+// Export User Router
+module.exports = userRouter
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const session = require("express-session")
+const app = express()
+require("dotenv").config()
+
+// Database Config
+mongoose.connect(process.env.DATABASE_URL, {
+
+})
+
+// Database Connection Error / Success
+const db = mongoose.connection
+db.on('error', (err) => console.log(err.message + ' is mongod not running?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+// Middlewares
+app.use(express.urlencoded({extended: true}))
+app.use(
+    session({
+        secret: process.env.SECRET,
+        resave: false,
+        saveUninitialized: false,
+    })
+)
+
+// Routes / Controllers
+const userController = require("./controllers/users")
+app.use("/users", userController)
+
+// Listener
+const PORT = process.env.PORT
+app.listen(PORT, () => console.log(`listening on port ${PORT}`))
